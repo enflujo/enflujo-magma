a94879da877f7089b54447012d9877d15bc10ac9
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2025-08-20 13:35:20 -0500

    🚧 En construcción

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // base: '/',
+  server: {
+    port: 3000,
+  },
+  publicDir: 'estaticos',
+  build: {
+    outDir: 'docs',
+    assetsDir: 'estaticos',
+    sourcemap: true,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
+});
